663311fd2ca9e01e8315eb704e4a8d27a5b48cf4
Arup Maity <arupmaity079@gmail.com>
2025-07-16 11:21:37 +0530

    added api on task page

--- src/app/(dashboard)/tasks/page.tsx
+++ src/app/(dashboard)/tasks/page.tsx
@@ -1,17 +1,23 @@
 'use client'
 import EditTask from '@/components/task-page/edit-task'
 import { Button } from '@/components/ui/button'
+import { axiosInstance } from '@/lib/axios'
+import { useQuery } from '@tanstack/react-query'
 import { Plus } from 'lucide-react'
 import React from 'react'
 
 const TasksPage = () => {
    const [openEditTask, setOpenEditTask] = React.useState(false)
-   const [selectedTaskID, setSelectedTaskID] = React.useState(null)
+   const [selectedTaskID, setSelectedTaskID] = React.useState("")
+   const { data, isLoading } = useQuery({
+      queryKey: ['tasks-list'],
+      queryFn: () => axiosInstance.get(`/tasks/all-tasks`).then(res => res.data)
+   })
    return (
       <>
          <div className='p-4'>
             <div className="border rounded-2xl p-6">
-               <div className="flex items-center justify-between">
+               <div className="flex items-center justify-between mb-8">
                   <h2 className="text-2xl font-semibold">Task List</h2>
                   <Button
                      onClick={() => setOpenEditTask(prev => !prev)}
@@ -20,11 +26,38 @@ const TasksPage = () => {
                      Add Task
                   </Button>
                </div>
+               <div className="">
+                  <table className='w-full'>
+                     <thead>
+                        <tr>
+                           <th className='w-80 text-left p-2 border'>Task Name</th>
+                           <th className='w-auto text-left p-2 border'>Description</th>
+                           <th className='w-auto text-left p-2 border'>Status</th>
+                           <th className='w-60 text-left p-2 border'>Options</th>
+                        </tr>
+                     </thead>
+                     <tbody>
+                        {
+                           isLoading ? <tr><td>Loading...</td></tr> :
+                              data?.length === 0 ?
+                                 <tr><td>No Task Found</td></tr> :
+                                 data?.map((task: any) => (
+                                    <tr key={task._id}>
+                                       <td className='w-80 text-left p-2 border'>{task.title}</td>
+                                       <td className='w-auto text-left p-2 border'>{task.description}</td>
+                                       <td className='w-auto text-left p-2 border'>{task.status}</td>
+                                       <td className='w-60 text-left p-2 border'>Options</td>
+                                    </tr>
+                                 ))
+                        }
+                     </tbody>
+                  </table>
+               </div>
             </div>
          </div>
          <EditTask
             open={openEditTask}
-            close={() => { setOpenEditTask(prev => !prev); setSelectedTaskID(null) }}
+            close={() => { setOpenEditTask(prev => !prev); setSelectedTaskID("") }}
             taskID={selectedTaskID}
          />
       </>
--- src/components/task-page/edit-task.tsx
+++ src/components/task-page/edit-task.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { z } from "zod";
 import TextareaAutosize from 'react-textarea-autosize';
-import { SubmitHandler, useForm } from "react-hook-form";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from "@/components/ui/sheet"
@@ -9,26 +9,60 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@
 
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
-import { MultiSelect } from "../common/multiple-select";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { axiosInstance } from "@/lib/axios";
+// import { MultiSelect } from "../common/multiple-select";
 
 const schemaValidation = z.object({
-   firstName: z.string(),
-   lastName: z.string(),
-   email: z.string().email(),
-   password: z.string().min(6),
+   title: z.string(),
+   description: z.string(),
+   status: z.string(),
 });
 
 type Inputs = z.infer<typeof schemaValidation>;
 
-const EditTask = ({ open, close, taskID }: { open: boolean; close: () => void; taskID?: number | null }) => {
-   const defaultValues = { firstName: "", lastName: "", email: "", password: "" };
+const EditTask = ({ open, close, taskID }: { open: boolean; close: () => void; taskID?: string }) => {
+   const defaultValues = { title: "", description: "", status: "todo" };
 
-   const { register, handleSubmit, reset, formState: { errors }, } = useForm<Inputs>({
+   const { register, control, handleSubmit, reset, setValue, formState: { errors }, } = useForm<Inputs>({
       defaultValues,
       mode: "onChange",
       resolver: zodResolver(schemaValidation)
    })
-   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+   const createMutation = useMutation({
+      mutationKey: ['create-task'],
+      mutationFn: (data: Inputs) => axiosInstance.post('/tasks/create-task', data).then(res => res.data),
+      onSuccess: () => {
+         close();
+         reset();
+      },
+      onError: (error) => {
+         console.log(error)
+      }
+   })
+   const updateMutation = useMutation({
+      mutationKey: ['create-task'],
+      mutationFn: (data: Inputs) => axiosInstance.put(`/tasks/update-task/${taskID}`, data).then(res => res.data),
+      onSuccess: () => {
+         close();
+         reset();
+      },
+      onError: (error) => {
+         console.log(error)
+      }
+   })
+   const { isLoading } = useQuery({
+      queryKey: ['read-task'],
+      queryFn: () => axiosInstance.get(`/tasks/read-task/${taskID}`).then(res => {
+         for (const key in defaultValues) {
+            setValue(key as keyof Inputs, res.data[key])
+         }
+         return res.data
+      }),
+      enabled: !!taskID,
+   })
+   console.log(errors)
+   const onSubmit: SubmitHandler<Inputs> = (data) => taskID ? updateMutation.mutate(data) : createMutation.mutate(data)
    return (
       <>
          <Sheet open={open} onOpenChange={() => { close(); reset() }}>
@@ -39,55 +73,70 @@ const EditTask = ({ open, close, taskID }: { open: boolean; close: () => void; t
                   </SheetTitle>
                   <SheetDescription className='hidden'></SheetDescription>
                </SheetHeader>
-               <form onSubmit={handleSubmit(onSubmit)} className='flex-grow flex flex-col overflow-y-auto'>
-                  <div className="grow overflow-y-auto">
-                     <div className="space-y-4 px-4">
+               {
+                  isLoading ?
+                     <div className=""></div> :
+                     <form onSubmit={handleSubmit(onSubmit)} className='flex-grow flex flex-col overflow-y-auto'>
+                        <div className="grow overflow-y-auto">
+                           <div className="space-y-4 px-4">
 
-                        <div className="flex flex-wrap -m-2">
-                           <fieldset className="w-7/12 p-2">
-                              <label className="block text-sm font-medium text-gray-900 mb-1.5">Title</label>
-                              <Input {...register('firstName')} />
-                              {errors.firstName && <span>{errors.firstName.message}</span>}
-                           </fieldset>
-                           <fieldset className="w-5/12 p-2">
-                              <label className="block text-sm font-medium text-gray-900 mb-1.5">Status</label>
-                              <Select>
-                                 <SelectTrigger className="w-80">
-                                    <SelectValue placeholder="Status" />
-                                 </SelectTrigger>
-                                 <SelectContent>
-                                    <SelectItem value="todo">todo</SelectItem>
-                                    <SelectItem value="in-progress">in-progress</SelectItem>
-                                    <SelectItem value="done">done</SelectItem>
-                                 </SelectContent>
-                              </Select>
-                              {errors.email && <span>{errors.email.message}</span>}
-                           </fieldset>
-                           <fieldset className="w-full p-2">
-                              <label className="block text-sm font-medium text-gray-900 mb-1.5">Description</label>
-                              <TextareaAutosize minRows={5} className="w-full border border-gray-300 p-2 rounded-md" />
-                              {errors.lastName && <span>{errors.lastName.message}</span>}
-                           </fieldset>
-                           <fieldset className="w-full p-2">
+                              <div className="flex flex-wrap -m-2">
+                                 <fieldset className="w-7/12 p-2">
+                                    <label className="block text-sm font-medium text-gray-900 mb-1.5">Title</label>
+                                    <Input {...register('title')} />
+                                    {errors.title && <span>{errors.title.message}</span>}
+                                 </fieldset>
+                                 <fieldset className="w-5/12 p-2">
+                                    <label className="block text-sm font-medium text-gray-900 mb-1.5">Status</label>
+                                    <Controller
+                                       name="status"
+                                       control={control}
+                                       render={({ field }) => (
+                                          <Select value={field.value} onValueChange={field.onChange}>
+                                             <SelectTrigger className="w-80">
+                                                <SelectValue placeholder="Status" />
+                                             </SelectTrigger>
+                                             <SelectContent>
+                                                <SelectItem value="todo">todo</SelectItem>
+                                                <SelectItem value="in-progress">in-progress</SelectItem>
+                                                <SelectItem value="done">done</SelectItem>
+                                             </SelectContent>
+                                          </Select>
+                                       )}
+                                    />
+                                 </fieldset>
+                                 <fieldset className="w-full p-2">
+                                    <label className="block text-sm font-medium text-gray-900 mb-1.5">Description</label>
+                                    <Controller
+                                       name="description"
+                                       control={control}
+                                       render={({ field }) => (
+                                          <TextareaAutosize value={field.value} onChange={(e) => field.onChange(e.target.value)} minRows={5} className="w-full border border-gray-300 p-2 rounded-md" />
+                                       )}
+                                    />
+                                    {errors.description && <span>{errors.description.message}</span>}
+                                 </fieldset>
+                                 {/* <fieldset className="w-full p-2">
                               <label className="block text-sm font-medium text-gray-900 mb-1.5">tags</label>
                               <MultiSelect />
                               {errors.lastName && <span>{errors.lastName.message}</span>}
-                           </fieldset>
+                           </fieldset> */}
+                              </div>
+                           </div>
                         </div>
-                     </div>
-                  </div>
-                  <div className="flex gap-2 p-4">
-                     <Button type='submit' className='w-32'
-                     >
-                        {taskID ? 'Update' : 'Add'}
-                     </Button>
-                     <Button variant='secondary' className='w-32'
-                        onClick={() => { close(); reset() }}
-                     >
-                        Cancel
-                     </Button>
-                  </div>
-               </form>
+                        <div className="flex gap-2 p-4">
+                           <Button type='submit' className='w-32'
+                           >
+                              {taskID ? 'Update' : 'Add'}
+                           </Button>
+                           <Button variant='secondary' className='w-32'
+                              onClick={() => { close(); reset() }}
+                           >
+                              Cancel
+                           </Button>
+                        </div>
+                     </form>
+               }
             </SheetContent>
          </Sheet>
       </>
